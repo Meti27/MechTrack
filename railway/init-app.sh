b2ac4b81eaa14d46ee722534c0d323d5d6330583
Muhamed Iseni <metiseni27@github.com>
2026-02-03 19:07:16 -0100

    Generated a new key

--- railway/init-app.sh
+++ railway/init-app.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
 
+php artisan optimize:clear
+
+php artisan storage:link || true
 php artisan migrate --force
 
-php artisan optimize:clear
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
